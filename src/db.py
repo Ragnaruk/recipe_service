600d5ecb4387735b797d219d582bd290d563716c
Ragnaruk <ima1365@me.com>
2020-08-30 17:15:54 +0300

    Added additional logging Added cache to db connection getter Added force_recreate option to db_fill

--- src/db.py
+++ src/db.py
@@ -8,11 +8,14 @@ import aiosqlite
 import sqlite3
 import json
 
+from aiohttp_cache import cache
 from pathlib import Path
 
+from src.log import logger
 from data.config import DB_PATH, FILE_PATH
 
 
+@cache()
 async def get_db(db_path: Path = DB_PATH) -> aiosqlite.Connection:
     """
     Get aiosqlite connection proxy.
@@ -32,6 +35,10 @@ async def execute_query(query: str, parameters: tuple = (), db_path: Path = DB_P
     :param db_path: path to database.
     :return: query results.
     """
+    logger.debug(
+        "Executing query: '{}' with parameters: '{}'".format(query, parameters)
+    )
+
     async with await get_db(db_path) as db:
         await db.execute(query, parameters)
         await db.commit()
@@ -48,18 +55,23 @@ async def get_query_results(
     :param db_path: path to database.
     :return: query results.
     """
+    logger.debug(
+        "Executing query: '{}' with parameters: '{}'".format(query, parameters)
+    )
+
     async with await get_db(db_path) as db:
         async with await db.execute(query, parameters) as cursor:
             return await cursor.fetchall()
 
 
-def fill_db(file_path: Path = FILE_PATH, db_path: Path = DB_PATH) -> bool:
+def db_fill(file_path: Path = FILE_PATH, db_path: Path = DB_PATH, force_recreate: bool = False) -> bool:
     """
     Create a database and transfer data from text file to it.
     Does nothing if db already exists.
 
     :param file_path: path to file with data.
     :param db_path: path to database.
+    :param force_recreate: force recreate database.
     :return: True if db was created, False if it already exists.
     """
 
@@ -67,7 +79,9 @@ def fill_db(file_path: Path = FILE_PATH, db_path: Path = DB_PATH) -> bool:
         with file_path.open(encoding="UTF-8") as f:
             return json.load(f)["recipes"]
 
-    if db_path.is_file():
+    if force_recreate:
+        db_remove()
+    elif db_path.is_file():
         return False
 
     db_path.touch()
@@ -108,5 +122,10 @@ def fill_db(file_path: Path = FILE_PATH, db_path: Path = DB_PATH) -> bool:
     return True
 
 
+def db_remove(db_path: Path = DB_PATH):
+    if db_path.is_file():
+        db_path.unlink()
+
+
 if __name__ == "__main__":
-    fill_db()
+    db_fill()
